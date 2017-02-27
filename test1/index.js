6dd90dbb0e54114b375497836730c73699901ad9
Markus Klinga <markus@woorank.com>
2017-02-27 17:02:28 +0100

    Add spinner

--- test1/index.js
+++ test1/index.js
@@ -1,7 +1,12 @@
 const Rx = window.Rx
 
+const wikiUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&utf8=1&srsearch='
+
 const searchInput = document.getElementById('search')
 const inputChange = Rx.Observable.fromEvent(searchInput, 'input')
+
+const spin = () =>
+  document.getElementById('content').innerHTML = '<ul><li>(imagine spinner here)</li></ul>';
 
 const render = (data) => {
   const content = document.getElementById('content')
@@ -18,8 +23,7 @@ const render = (data) => {
   content.parentElement.replaceChild(newContent, content)
 }
 
-const wikiUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&utf8=1&srsearch='
-
+// Set the data flow from input -> fetch -> render
 inputChange
   // Get the text value of the raw event
   .map(event => event.target.value)
@@ -29,6 +33,9 @@ inputChange
 
   // Send query at maximum every 500ms
   .debounceTime(500) 
+
+  // Set spinner
+  .do(spin)
 
   // FlatMap is like a map that allows its result to send new events
   .flatMap(value => Rx.Observable.fromPromise(fetch(`${wikiUrl}${value}`)))
